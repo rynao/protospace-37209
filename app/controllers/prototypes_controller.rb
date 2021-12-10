3d4953b97c045f88fd3ae27fe2e077c2dc2a0ba5
rynao <94769118+rynao@users.noreply.github.com>
2021-12-11 08:59:16 +0900

    プロトタイプ編集・削除機能の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -20,6 +20,25 @@ class PrototypesController < ApplicationController
     @prototype = Prototype.find(params[:id])
   end
 
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    prototype = Prototype.find(params[:id])
+    if prototype.update(proto_params)
+      redirect_to prototype_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
   private
   def proto_params
     params.require(:prototype).permit([:title, :catch_copy, :concept, :image]).merge(user_id: current_user.id)
