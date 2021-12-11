83a1c8bba72fab22d5c3de1dce620b6a4ac0d7a9
rynao <94769118+rynao@users.noreply.github.com>
2021-12-11 13:20:59 +0900

    ユーザーのアクセス範囲の制限実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,5 @@
 class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   def index
     @prototypes = Prototype.all
   end
@@ -24,6 +25,9 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
+    unless current_user == @prototype.user
+      redirect_to action: :index
+    end
   end
 
   def update
